1cabf09eae708cbe530b35a950018a511e82c7f5
Yash Adhikari <yash.adhikari@office.mypay.com.np>
2022-09-30 12:58:34 +0545

    QR creater

---
+++ QR/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using QR.Models;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+       TicketDetail tick = new TicketDetail();
+        
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Privacy(TicketDetail ticketDetail)
+
+        {
+            var jsonString = JsonConvert.SerializeObject(ticketDetail);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(jsonString, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                using (Bitmap bitmap = qrCode.GetGraphic(20))
+                {
+                    bitmap.Save(ms, ImageFormat.Png);
+                    ViewBag.qrCodeImage = "data:image/Png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+
+
+
+
+            return View(ticketDetail);
+        }
+
+
+
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QR/Models/TicketDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QR.Models
+{
+    public class TicketDetail
+    {
+        public int Id { get; set; } = 2;
+        public string Name { get; set; } = "Yash adhikari";
+        public string Event { get; set; } = "FootBall Match";
+        public string Venue { get; set; } = "FootBall Ground";
+        public DateTime EventDate { get; set; } 
+        public int price { get; set; } = 500;
+
+    }
+}
